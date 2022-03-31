bae05f3d974b430a7c4c9e7e8916836f357c1527
Mayank-Patel1 <Mayank-Patel1@users.noreply.github.com>
2022-03-31 19:43:59 -0400

    Adding logo

--- wareHouseApp/App.js
+++ wareHouseApp/App.js
@@ -1,9 +1,18 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
+      <Image source = {require('../images/logo.PNG')} />
       <Text>Open up mayank to start working on your app!</Text>
       <StatusBar style="auto" />
     </View>
@@ -13,7 +22,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: 'white',
     alignItems: 'center',
     justifyContent: 'center',
   },
